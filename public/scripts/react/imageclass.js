f673753282fe85e8f7a9238941c993db8c8b1b62
yiyang89 <yiyangkok@gmail.com>
2017-04-24 04:36:06 -0700

    swapped handwritten filter for built in js filter function

--- public/scripts/react/imageclass.js
+++ public/scripts/react/imageclass.js
@@ -5,6 +5,7 @@ var ImageComponent = React.createClass({
     // Display lightbox on click.
     // return <img src={this.props.imageurl} onError={(e)=>{e.target.src=PLACEHOLDER_IMAGE}}/>;
     // Lightbox source downloaded from http://lokeshdhakar.com/projects/lightbox2/
+    // Placeholder image courtesy of http://www.jennybeaumont.com/post-placeholders/
     var caption = this.props.caption + " - " + this.props.postedby;
     return <a href={this.props.imageurl} data-lightbox={this.props.imageurl} data-title={caption}><img src={this.props.imageurl} onError={(e)=>{e.target.src=PLACEHOLDER_IMAGE}}/></a>;
   }
--- public/scripts/react/mosiacclass.js
+++ public/scripts/react/mosiacclass.js
@@ -1,37 +1,21 @@
 var MosaicComponent = React.createClass({
-  // fisher yates shuffle from: https://www.frankmitchell.org/2015/01/fisher-yates/
-  // shuffle: function(array) {
-  //   var i = 0
-  //   var j = 0
-  //   var temp = null
-  //
-  //   for (i = array.length - 1; i > 0; i -= 1) {
-  //     j = Math.floor(Math.random() * (i + 1))
-  //     temp = array[i]
-  //     array[i] = array[j]
-  //     array[j] = temp
-  //   }
-  // },
   render: function() {
     // Filter here for even distribution.
     // My uploads
     // My Pins
     // Target user uploads
     var datacopy = this.props.data.slice();
-    // this.shuffle(datacopy);
-    var indextoremove = [];
-    for (var i = 0; i < datacopy.length; i++) {
-      if (this.props.mypins && (!datacopy[i].likeData.includes(this.props.username))) {
-        indextoremove.push(i);
-      } else if (this.props.myuploads && (datacopy[i].postedby !== this.props.username)) {
-        indextoremove.push(i);
-      } else if (this.props.useruploads && (datacopy[i].postedby !== this.props.uploadedname)) {
-        indextoremove.push(i)
-      }
-    }
-    for (var i = indextoremove.length - 1; i >= 0; i--) {
-      datacopy.splice(indextoremove[i], 1);
-    }
+    datacopy = datacopy.filter(function(carddata) {
+        if (this.props.mypins && (!carddata.likeData.includes(this.props.username))) {
+          return false;
+        } else if (this.props.myuploads && (carddata.postedby !== this.props.username)) {
+          return false;
+        } else if (this.props.useruploads && (carddata.postedby !== this.props.uploadedname)) {
+          return false
+        } else {
+          return true;
+        }
+    }.bind(this))
     var masonrytemp = {'0': [0, []], '1':[0, []], '2':[0, []], '3':[0, []], '4': [0, []]};
     // Push into the column with the smallest height.
     for (var i = 0; i < datacopy.length; i++) {
@@ -58,12 +42,6 @@ var MosaicComponent = React.createClass({
       }
     }
 
-
-    // for (var i = 0; i < datacopy.length; i++) {
-    //   masonrytemp[i % 5].push(
-    //     <CardComponent carddata={datacopy[i]} key={i} username={this.props.username} uploadedname={this.props.uploadedname} pinfunc={this.props.pinfunc} unpinfunc={this.props.unpinfunc} deletefunc={this.props.deletefunc} showmypins={this.props.mypins} showmyuploads={this.props.myuploads} showuseruploads={this.props.useruploads}/>
-    //     );
-    // }
     return (
       <div className="grid-by-columns">
         <div className="grid-by-rows">{masonrytemp[0][1]}</div>
@@ -74,26 +52,4 @@ var MosaicComponent = React.createClass({
       </div>
       );
     }
-
-  // render: function() {
-  //   // Divide items between 5 columns.
-  //   console.log("rerendering mosaic");
-  //   var masonrytemp = {'0': [], '1':[], '2':[], '3':[], '4': []};
-  //   for (var i = 0; i < this.props.data.length; i++) {
-  //     masonrytemp[i % 5].push(
-  //       <CardComponent carddata={this.props.data[i]} key={i} username={this.props.username} uploadedname={this.props.uploadedname} pinfunc={this.props.pinfunc} unpinfunc={this.props.unpinfunc} deletefunc={this.props.deletefunc} showmypins={this.props.mypins} showmyuploads={this.props.myuploads} showuseruploads={this.props.useruploads}/>
-  //       );
-  //   }
-  //   return (
-  //     <div className="grid-by-columns">
-  //       <div className="grid-by-rows">{masonrytemp[0]}</div>
-  //       <div className="grid-by-rows">{masonrytemp[1]}</div>
-  //       <div className="grid-by-rows">{masonrytemp[2]}</div>
-  //       <div className="grid-by-rows">{masonrytemp[3]}</div>
-  //       <div className="grid-by-rows">{masonrytemp[4]}</div>
-  //     </div>
-  //     );
-  // }
 })
-
-// Placeholder image courtesy of http://www.jennybeaumont.com/post-placeholders/
